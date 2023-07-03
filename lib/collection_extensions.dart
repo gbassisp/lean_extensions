50f757a4d57097bf625095f7a4c10ef04b801bcb
gbassisp <guilherme.bassi.sp@gmail.com>
2023-07-03 17:42:04 +0930

    fix: move collection export into separate library

---
+++ lib/collection_extensions.dart
@@ -0,0 +1,15 @@
+/// re-exports extensions from the collection library
+library collection_extensions;
+
+export 'package:collection/collection.dart'
+    show
+        ComparatorExtension,
+        IterableComparableExtension,
+        IterableDoubleExtension,
+        IterableExtension,
+        IterableIntegerExtension,
+        IterableIterableExtension,
+        IterableNullableExtension,
+        IterableNumberExtension,
+        ListComparableExtensions,
+        ListExtensions;
--- lib/lean_extensions.dart
+++ lib/lean_extensions.dart
@@ -2,17 +2,5 @@
 /// dependencies.
 library lean_extensions;
 
-export 'package:collection/collection.dart'
-    show
-        ComparatorExtension,
-        IterableComparableExtension,
-        IterableDoubleExtension,
-        IterableExtension,
-        IterableIntegerExtension,
-        IterableIterableExtension,
-        IterableNullableExtension,
-        IterableNumberExtension,
-        ListComparableExtensions,
-        ListExtensions;
 export 'src/converters.dart';
 export 'src/extensions.dart';
