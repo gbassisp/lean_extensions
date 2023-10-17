6e109a6d5aa9c0d51afb041481a98c9bd82c2047
guilherme <guilherme.bassi.sp@gmail.com>
2023-10-17 21:55:49 +1030

    feat: added simple eval() function

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,3 +1,34 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:isolate';
+
 /// wrapper around Future.delayed to have a simpler syntax
-Future<void> sleep(double seconds) =>
+Future<void> sleep(num seconds) =>
     Future.delayed(Duration(microseconds: (seconds * 1000000).toInt()));
+
+/// evaluates a dart expression
+Future<T> eval<T>(String expression, {T Function(String raw)? decoder}) async {
+  final dec = decoder ?? jsonDecode;
+  final uri = Uri.dataFromString(
+    '''
+    import "dart:isolate";
+    import "package:lean_extensions/collection_extensions.dart";
+    import "package:lean_extensions/dart_essentials.dart";
+    import "package:lean_extensions/lean_extensions.dart";
+
+    void main(_, SendPort port) {
+      port.send($expression);
+    }
+    ''',
+    mimeType: 'application/dart',
+  );
+
+  final port = ReceivePort();
+  final isolate = await Isolate.spawnUri(uri, [], port.sendPort);
+  final response = (await port.first).toString();
+
+  port.close();
+  isolate.kill();
+
+  return dec(response) as T;
+}
--- test/src/others_test.dart
+++ test/src/others_test.dart
@@ -1,15 +1,23 @@
-import 'package:lean_extensions/src/utils.dart';
+import 'package:lean_extensions/dart_essentials.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('utils.dart', () {
     test('sleep()', () async {
       final a = Stopwatch()..start();
-      await sleep(10);
+      const t = 5;
+      await sleep(t);
       a.stop();
 
       // allow 0.5s for start() and stop()
-      expect(a.elapsedMilliseconds, closeTo(10000, 500));
+      expect(a.elapsedMilliseconds, closeTo(t * 1000, 500));
+    });
+
+    test('eval()', () async {
+      const a = 'range(10).toList()';
+      final res = await eval<List<dynamic>>(a);
+
+      expect(res, containsAllInOrder(range(10)));
     });
   });
 }
