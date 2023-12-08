23987faf305ab8604883cf75c830a24860e09fda
gbassisp <guilherme.bassi.sp@gmail.com>
2023-12-08 22:24:58 +1030

    fix: remove use of reserved keyword

--- lib/src/numeral_system.dart
+++ lib/src/numeral_system.dart
@@ -4,10 +4,10 @@ const _base64 = '0123456789'
     '+/';
 const _size = _base64.length;
 
-String _toRadixString(BigInt number, int base) {
-  final valid = base >= 2 && base <= _size;
+String _toRadixString(BigInt number, int radix) {
+  final valid = radix >= 2 && radix <= _size;
   if (!valid) {
-    throw RangeError('Invalid value: Not in inclusive range 2..$_size: $base');
+    throw RangeError('Invalid value: Not in inclusive range 2..$_size: $radix');
   }
 
   if (number == BigInt.zero) {
@@ -16,7 +16,7 @@ String _toRadixString(BigInt number, int base) {
 
   var res = '';
   var n = number.abs();
-  final b = BigInt.from(base);
+  final b = BigInt.from(radix);
   while (n > BigInt.zero) {
     final r = (n % b).toInt();
     final c = _base64[r];
@@ -29,8 +29,8 @@ String _toRadixString(BigInt number, int base) {
 
 /// simple implementation of a number system conversion;
 /// from base 10 to n, where n is between 2 and 64 inclusive
-/// based on https://en.wikipedia.org/wiki/Base62 and 
+/// based on https://en.wikipedia.org/wiki/Base62 and
 /// https://onlinelibrary.wiley.com/doi/abs/10.1002/spe.408
-String toRadix(Object number, int base) {
-  return _toRadixString(BigInt.parse('$number'), base);
+String toRadix(Object number, int radix) {
+  return _toRadixString(BigInt.parse('$number'), radix);
 }
