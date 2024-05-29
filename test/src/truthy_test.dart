392c3fd8ba62374fd7efa3725807da5cba64f14a
guilherme <guilherme.bassi.sp@gmail.com>
2024-05-29 21:27:26 +0930

    test: organise truthy test cases

--- test/src/truthy_test.dart
+++ test/src/truthy_test.dart
@@ -1,38 +1,49 @@
 import 'package:lean_extensions/dart_essentials.dart';
+import 'package:lean_extensions/src/range.dart';
 import 'package:test/test.dart';
 
 import 'test_utils.dart';
 
+final falsy = [
+  null,
+  0,
+  '',
+  <dynamic>[],
+  <dynamic>{},
+  <dynamic, dynamic>{},
+  range(0),
+];
+final truthy = [
+  'a',
+  1,
+  [1],
+  {1},
+  {1: 1},
+  'true',
+  range(1),
+];
+const opinionatedFalsy = [
+  'zero',
+  '0',
+  '0.',
+  '0.0',
+  'false',
+  'False',
+  'FALSE',
+  'null',
+  ' ',
+];
+
 void main() {
   group('truthy / falsy', () {
-    test('truthy', () {
-      expect('a', isTruthy);
-      expect(1, isTruthy);
-      expect([1], isTruthy);
-      expect({1}, isTruthy);
-      expect({1: 1}, isTruthy);
-      expect('true', isTruthy);
-      expect(range(1), isTruthy);
-    });
-    test('falsy', () {
-      expect(null, isFalsy);
-      expect('', isFalsy);
-      expect(0, isFalsy);
-      expect(<dynamic>[], isFalsy);
-      expect(<dynamic>{}, isFalsy);
-      expect(<dynamic, dynamic>{}, isFalsy);
-      expect(range(0), isFalsy);
-    });
-    test('opinionated truthiness', () {
-      expect('zero', isFalsy);
-      expect('0', isFalsy);
-      expect('0.', isFalsy);
-      expect('0.0', isFalsy);
-      expect('false', isFalsy);
-      expect('False', isFalsy);
-      expect('FALSE', isFalsy);
-      expect('null', isFalsy);
-      expect(' ', isFalsy);
-    });
+    for (final t in truthy) {
+      test('$t has truthy value', () => expect(t, isTruthy));
+    }
+    for (final f in falsy) {
+      test('$f has falsy value', () => expect(f, isFalsy));
+    }
+    for (final f in opinionatedFalsy) {
+      test('$f has falsy value', () => expect(f, isFalsy));
+    }
   });
 }
